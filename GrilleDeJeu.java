37bf2268ed2246170c549f22ac38fb3e26fec0e4
Shubham Dhingra <54913662+ShubhamDhingra96@users.noreply.github.com>
2020-10-22 23:34:29 -0400

    Update GrilleDeJeu.java

--- GrilleDeJeu.java
+++ GrilleDeJeu.java
@@ -2,33 +2,18 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-/*
-	GrilleDeJeu représente une grille de tic tac toe sur laquelle
-	on peut cliquer pour choisir la position du prochain X.
-
-	Les positions sur la grille sont indexées de la façon suivante :
-
-	0	1	2
-	3	4	5
-	6	7	8
-
-	La classe GrilleDeJeu ne gère que l'affichage et le choix du joueur X.
-	L'interface TicTacToe déclare les méthodes qui seront utilisées par GrilleDeJeu
-	pour communiquer avec la classe qui va gérer le jeu.
- */
-
 public class GrilleDeJeu extends JFrame implements ActionListener{
 
-    private TicTacToe unJeu;		//Référence sur le jeu
-    private JButton[] tab;	//Tableau pour conserver les références sur
-    						//les boutons
-    private int[] tabGagne; //Un tableau qui contiendra les indices de
-    						//la combinaison gagnante
-    private boolean fini;	//Le jeu est fini
-    private Color couleur; 	//Pour conserver la couleur par défaut des boutons
-    private JLabel message;	//Afficher le gagnant ou partie nulle
+    private TicTacToe unJeu;		
+    private JButton[] tab;
+    			
+    private int[] tabGagne; 
+    			
+    private boolean fini;	
+    private Color couleur; 	
+    private JLabel message;	
 
-    //Constructeur pour créer l'interface graphique
+    
     public GrilleDeJeu() {
     	int i;
     	JButton bouton;
@@ -72,7 +57,7 @@ public class GrilleDeJeu extends JFrame implements ActionListener{
 		initialiser();
     }
 
-    //Rétablir l'aspect initial de la grille
+    
     private void initialiser(){
     	for( int i = 0; i<9; i++){
     		tab[i].setBackground(couleur);
@@ -85,8 +70,8 @@ public class GrilleDeJeu extends JFrame implements ActionListener{
     	message.setText("");
     }
 
-    //Méthode de l'interface ActionListener qui va être appelée lors
-    //du click sur un bouton.
+   
+   
     public void actionPerformed (ActionEvent evt) {
     	String arg = evt.getActionCommand();
     	JButton bouton = (JButton)evt.getSource();
@@ -98,22 +83,22 @@ public class GrilleDeJeu extends JFrame implements ActionListener{
 
     }
 
-	//Le bouton à l'index i a été cliqué
+
     void boutonClique(int i){
     	if(fini)
     		return;
-    	unJeu.setX(i);				//Transmettre l'index du choix X au jeu
-    	if(unJeu.gagnant("X",tabGagne)){	//Victoire de X?
+    	unJeu.setX(i);				
+    	if(unJeu.gagnant("X",tabGagne)){	
     		fini = true;
     		marque(tabGagne);
     		message.setText("X gagne!");
     	}
     	else
-    		if(!unJeu.isPartieNulle()){				 // Si pas victoire de X et pas nulle
-    			int cellule = unJeu.getO(); // Demande le choix pour O
-    			tab[cellule].setText("O");		 // Modifier l'affichage sur la grille pour
-    											 // réfléter le choix de O
-    			if(unJeu.gagnant("O",tabGagne)){	 // O gagne?
+    		if(!unJeu.isPartieNulle()){			
+    			int cellule = unJeu.getO(); 
+    			tab[cellule].setText("O");
+    						
+    			if(unJeu.gagnant("O",tabGagne)){	 
     				fini = true;
     				marque(tabGagne);
     				message.setText("O gagne!");
@@ -126,7 +111,7 @@ public class GrilleDeJeu extends JFrame implements ActionListener{
     		}
     }
 
-    //changer la couleur des boutons de la combinaison gagnante
+    
     public void marque(int[] t){
     	for(int i=0; i<3;i++){
     		tab[t[i]].setBackground(Color.BLUE);
@@ -148,7 +133,7 @@ public class GrilleDeJeu extends JFrame implements ActionListener{
 
     	GrilleDeJeu g = new GrilleDeJeu();
 
-    	/*Exemple d'utilisation de testDebug avec jeu dans l'état
+    	/*Exemple d'utilisation de testDebug avec jeu dans l'Ã©tat
     	 *-------
     	 *|x| |O|
     	 *| |x|O|
